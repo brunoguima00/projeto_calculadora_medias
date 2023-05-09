d59074b65898c19bc9454b07c571f06ca5ea02bd
Bruno Guimarães <bruno.guima00@gmail.com>
2023-05-09 07:01:31 -0300

    fix Limitação das casas decimais da média

--- main.js
+++ main.js
@@ -49,7 +49,8 @@ function atualizaTabela(){
 function atualizaMediaFinal () {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    // tofixed() delimita as casas decimais
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2); 
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
 
